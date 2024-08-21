e75769ea8c584e28833617b4c8c3b706fe4b43fa
“Nathan <schroedernathan2011@icloud.com>
2024-08-20 20:54:29 -0400

    When clicking chip, function now triggers in the search component replacing the text

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -4,6 +4,7 @@ import { Field, Input } from "@headlessui/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Chip from "./chips/Chip";
+import { useState } from "react";
 
 // Temporary data for chips
 const chips = [
@@ -15,6 +16,14 @@ const chips = [
 ];
 
 export default function Search() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleChipClick = (name: string) => {
+    setSearchText(name);
+
+    // Filter cards based on searchText
+  };
+
   return (
     <>
       <Field className="relative sm:w-2/3 mx-auto mt-24 group/search mb-5">
@@ -24,12 +33,19 @@ export default function Search() {
             "block w-full rounded-full bg-background ring-1 ring-outline py-2 px-5 text-text placeholder:text-outline font-lora",
             "focus:outline-none hover:ring-2"
           )}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
         />
         <MagnifyingGlassIcon className="size-6 text-text absolute right-4 top-2 group-hover/search:-rotate-12 transition-transform" />
       </Field>
       <div className="flex flex-wrap gap-x-3 gap-y-3 justify-center">
         {chips.map((chip) => (
-          <Chip key={chip.id} id={chip.id} name={chip.name} /> // pass id and name as props to chip
+          <Chip
+            handleChipClick={handleChipClick}
+            key={chip.id}
+            id={chip.id}
+            name={chip.name}
+          /> // pass id and name as props to chip
         ))}
       </div>
     </>
--- src/app/components/chips/Chip.tsx
+++ src/app/components/chips/Chip.tsx
@@ -1,12 +1,21 @@
+"use client";
 import React from "react";
 
 // provide id and name as props
-const Chip = ({ id, name }: { id: number; name: string }) => {
+const Chip = ({
+  id,
+  name,
+  handleChipClick,
+}: {
+  id: number;
+  name: string;
+  handleChipClick: any;
+}) => {
+
+
   return (
-    <a key={id} href="#">
-      <div
-        className="rounded-full w-fit ring-1 ring-outline text-text font-lora px-3"
-      >
+    <a key={id} className="cursor-pointer" onClick={() => handleChipClick(name)}>
+      <div className="rounded-full w-fit ring-1 ring-outline text-text font-lora px-3">
         {name}
       </div>
     </a>
