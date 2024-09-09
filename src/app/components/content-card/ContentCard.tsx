c1a4c19355cfb7dfbfbf20d993880b24f8921c9a
“Nathan <schroedernathan2011@icloud.com>
2024-09-09 17:37:19 -0400

    Created functional flexbox layout

---
+++ src/app/components/content-card/ContentCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContentCard = () => {
+  return (
+    <div className="p-1  basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+      <div className="p-3 bg-white h-48">
+        ContentCard something
+      </div>
+    </div>
+  );
+};
+
+export default ContentCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Footer from "./components/footer/Footer";
-import NavBar from "./components/nav/NavBar";
+import ContentCard from "./components/content-card/ContentCard";
 import Search from "./components/Search";
 
 export default function Home() {
   return (
     <div className="bg-background">
-
       <div className="relative isolate px-6  lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl pt-32 sm:pt-48 lg:pt-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full text-[#262626]/60 dark:text-[#D9D9D9]/60 dark:ring-[#D9D9D9]/20 dark:hover:ring-[#D9D9D9]/30 px-3 py-1 leading-6 font-lora  ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Share your design’s with the world.{"    "}
@@ -26,6 +24,21 @@ export default function Home() {
             </p>
           </div>
           <Search />
+        </div>
+        {/* Content -- TODO: create filter buttons */}
+        <div className="mt-16 sm:mx-24">
+          <div className="bg-red-500 flex flex-wrap">
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+
+          </div>
         </div>
       </div>
     </div>
